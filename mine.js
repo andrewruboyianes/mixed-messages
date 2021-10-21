834f79f1e83dccfd724b21b84f2357f0af27ed5b
Andrew Ruboyianes <andrewrubos@gmail.com>
2021-10-20 20:38:16 -0700

    random number function

---
+++ mine.js
@@ -0,0 +1,7 @@
+const generateRandomQuote = () => {
+    const num = Math.floor(Math.random() * 5)
+    return num
+}
+
+console.log(generateRandomQuote())
+
--- my solution.js
+++
@@ -1,6 +0,0 @@
-const generateRandomQuote = () => {
-    const num = Math.floor(Math.random * 5)
-    console.log(num)
-}
-
-generateRandomQuote()
