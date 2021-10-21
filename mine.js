0fac103cb141337cd44860e0472dcd8fa43faf5b
Andrew Ruboyianes <andrewrubos@gmail.com>
2021-10-20 20:52:40 -0700

    generate random quote

--- mine.js
+++ mine.js
@@ -1,7 +1,27 @@
-const generateRandomQuote = () => {
-    const num = Math.floor(Math.random() * 5)
+const generateRandomNumber = () => {
+    const num = Math.floor(Math.random() * 4)
     return num
-}
+};
 
-console.log(generateRandomQuote())
+const randomQuote = () => {
+    switch(generateRandomNumber()) {
+        case 0:
+            console.log('"The day you becom esuccessful is th day you become humble -Naveen Jane"')
+            break;
+        case 1:
+            console.log('"A masterpiece is still a masterpiece even when the lights are off and the room is empty" -Unkown')
+            break;
+        case 2:
+            console.log('"The cost of procrastination is a life you could\'ve lived" -Unknown')
+            break;
+        case 3: 
+            console.log('"People don\'t care what you know unitll they know that you care" -Kevion')
+            break;
+        default:
+            console.log('Please select a random number.')
+    }
 
+
+};
+
+randomQuote()
